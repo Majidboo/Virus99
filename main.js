f534e1b93cf7e210a7763138c34779aaf46ae835
Majidboo <dalandangapr2@gmail.com>
2024-05-29 16:53:30 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let music = document.getElementById("missyou");
+function Black() {
+  document.body.style.background = " black";
+  music.play();
+  
+}
+
+function White(){
+  document.body.style.background = "white";
+  music.pause();
+}
